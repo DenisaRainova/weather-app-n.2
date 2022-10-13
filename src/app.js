f383985358cb1689e0a4c34c0c490890662f9802
DenisaRainova <Dendous@email.cz>
2022-10-13 12:45:32 +0200

    Created function displayTemperature with let apiUrl and axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  console.log(response.data);
+}
+
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
