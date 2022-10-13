142e01e6bbcb2c74fbcf8880041b337a4001405f
DenisaRainova <Dendous@email.cz>
2022-10-13 17:15:36 +0200

    Math.round

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 function displayTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
