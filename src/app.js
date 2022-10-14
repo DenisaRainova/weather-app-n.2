a8c6f0ab17d1bff45b5a25cea00112d20b25e4cf
DenisaRainova <Dendous@email.cz>
2022-10-14 15:42:53 +0200

    Change apiUrl

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
