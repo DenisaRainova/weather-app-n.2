f0f49eaec434a5974b73bd859db27764ff85f5be
DenisaRainova <Dendous@email.cz>
2022-10-14 20:51:01 +0200

    created basic fahrenheit click function

--- src/app.js
+++ src/app.js
@@ -63,7 +63,17 @@ function handleSubmit(event){
     search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event){
+  event.preventDefault();
+  alert("Link click");
+}
+
+
 search("New York");
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
