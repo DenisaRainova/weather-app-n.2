bf1d94ae78459ae48e0ad9cb4ca44de1ac97e222
DenisaRainova <Dendous@email.cz>
2022-10-14 21:38:42 +0200

    Styling C | F add and remove class active

--- src/app.js
+++ src/app.js
@@ -68,12 +68,18 @@ function handleSubmit(event){
 function displayFahrenheitTemperature(event){
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  //remove the active class from the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event){
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
