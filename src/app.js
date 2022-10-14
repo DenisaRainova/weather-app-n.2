c830353128819587058e1ae60dc8378c20987592
DenisaRainova <Dendous@email.cz>
2022-10-14 16:59:58 +0200

    Started with handleSubmit

--- src/app.js
+++ src/app.js
@@ -44,6 +44,11 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function handleSubmit(event){
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+}
+
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
 let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat={lat}&lon={lon}&appid=${apiKey}&units=metric`;
@@ -51,3 +56,6 @@ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lat={lat
 console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
