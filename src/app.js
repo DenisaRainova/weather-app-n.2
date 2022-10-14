59ade91f870943dc128ad0768b8905c3f9e4cead
DenisaRainova <Dendous@email.cz>
2022-10-14 14:40:58 +0200

    Add let city and description

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
