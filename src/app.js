f39dc3d65842d5020f408949bd1163cf5cc9f0a9
DenisaRainova <Dendous@email.cz>
2022-10-23 11:21:18 +0200

    Function getForecast

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ function displayForecast() {
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
 
-  let forecastHTML = `<div class="row">`;
+  let forecastHTML = `<div class="row">`; //Originally mistake "row>`
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -52,6 +52,14 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  
+  let apiKey = "b884720d8f959cc5aa66ad10c2546f56";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -75,6 +83,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city){
